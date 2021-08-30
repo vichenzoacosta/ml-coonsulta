73af49471b28a146d2fd9a3d0151277918d97c0c
vichenzoacosta <vichenzoacostab@gmail.com>
2021-08-29 21:18:54 -0300

    primer desafio levantamos servidor

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hola mundo");
+});
+app.listen(3000, () => {
+    console.log('App listening http://localhost:3000/');
+});
